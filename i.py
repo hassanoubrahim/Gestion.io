ca4b20411e6d234c74d3cf1615779c7e4aa53e7d
hassanoubrahim <65039956+hassanoubrahim@users.noreply.github.com>
2024-05-25 12:44:22 +0100

    Update i.py

--- i.py
+++ i.py
@@ -20,3 +20,11 @@ weights = [abs(edge[2]['weight']) for edge in edges]
 nx.draw_networkx(G, pos, with_labels=True, node_size=7000, node_color='lightblue', font_size=10, font_weight='bold', edge_color=weights, edge_cmap=plt.cm.viridis, width=3)
 plt.title('Network of Correlated Features')
 plt.show()
+
+# Add directed edges between highly correlated nodes only
+for i in corr_matrix.columns:
+    for j in corr_matrix.index:
+        if i != j and abs(corr_matrix.loc[i, j]) > threshold:
+            # Directing edge from lower indexed feature to higher indexed feature for simplicity
+            if corr_matrix.columns.get_loc(i) < corr_matrix.columns.get_loc(j):
+                G.add_edge(i, j, weight=corr_matrix.loc[i, j])
